5a1af0af63524b4cb87cfaee89a10dced2412f91
Taylor Huffman <taylor@crucialartists.com>
2022-05-18 16:27:32 -0500

    Complete assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'Brown', 'Calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 2, 'F', 'Black', 'Angry', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 4, 'F', 'Blue', 'Silly', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 1, 'M', 'Brown', 'Serious', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 3, 'F', 'Black', 'Happy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 9, 'M', 'Green', 'Annoyed', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 11, 'F', 'Brown', 'Shy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 3, 'M', 'Gray', 'Calm', 1);
